6a8aad4db0d36bb9f4c4d5a38a9660a375ef9c97
KingCobra29A <brunerk@umich.edu>
2022-12-09 12:29:31 -0500

    ready to deploy

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.post("/new", function (req, res) {
+  let { messageText, messageUser } = req.body;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
+});
+
+/* GET home page. */
+router.get("/new", function (req, res, next) {
+  res.render("form", {});
+});
+
+module.exports = router;
